21751428a9a467b6f38bc397c471fcf36a24b95f
PujaKumari23 <68163885+PujaKumari23@users.noreply.github.com>
2020-07-31 00:02:41 -0700

    Create EmailApp.java

---
+++ Email Generator/EmailApp.java
@@ -0,0 +1,12 @@
+package emailapp;
+
+public class EmailApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Email e1 =new Email("Puja", "Kumari");
+		System.out.println(e1.showinfo());
+
+	}
+
+}
